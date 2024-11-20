1885dfcd688d8312c944afe49e0748d40011056f
Baraka Kinywa <bkinywa24@gmail.com>
2024-11-20 13:58:30 +0200

    [NEW]layout.php: Adding a layout model

--- exercice-backend-php/views/home.php
+++ exercice-backend-php/views/home.php
@@ -1,13 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
-</head>
-<body>
+<?php
+$title = "Login";
+ob_start();
+?>
     <div class="container d-flex justify-content-center align-items-center" style="height: 100vh;">
         <div class="card p-4 shadow" style="width: 300px;">
             <h3 class="text-center mb-4">Login</h3>
@@ -24,5 +18,6 @@
             </form>
         </div>
     </div>
-</body>
-</html>
+    <?php
+$content = ob_get_clean();
+include 'layout.php';
